263f5e95beee0e20bb6debd613354497d5e48524
Olesia Kapustynska <d.olleska@gmail.com>
2024-08-01 22:31:18 +0300

    first commit

---
+++ src/main/java/com/example/newsparserapp/NewsParserAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsparserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsParserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsParserAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newsparserapp/NewsParserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsparserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsParserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
